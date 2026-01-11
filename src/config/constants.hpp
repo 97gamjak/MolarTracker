1b5aedc7524656832f807735ad778a34f42ad70f
Jakob Gamper <97gamjak@gmail.com>
2026-01-11 22:28:03 +0100

    feat: refactor application name and desktop file handling using constants

---
+++ src/config/constants.hpp
@@ -0,0 +1,12 @@
+#ifndef __CONFIG__CONSTANTS_HPP__
+#define __CONFIG__CONSTANTS_HPP__
+
+struct Constants
+{
+    // clang-format off
+    static constexpr const char* APP_NAME = "Molar Tracker";
+    static constexpr const char* DESKTOP_APP_NAME = MOLARTRACKER_DESKTOP_APP_NAME;
+    // clang-format on
+};
+
+#endif   // __CONFIG__CONSTANTS_HPP__
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <QApplication>
 
 #include "app/app_context.hpp"
+#include "config/constants.hpp"
 #include "ui/main_window.hpp"
 
 int main(int argc, char** argv)
 {
     QApplication app{argc, argv};
 
-    app.setApplicationName("Molar Tracker");
-    app.setApplicationDisplayName("Molar Tracker");
-    app.setOrganizationName("Molar Tracker Contributors");
+    app.setApplicationName(Constants::APP_NAME);
+    app.setApplicationDisplayName(Constants::APP_NAME);
 
-    app.setDesktopFileName(MOLARTRACKER_DESKTOP_APP_NAME);
+    app.setDesktopFileName(Constants::DESKTOP_APP_NAME);
 
     app::AppContext appContext;
     ui::MainWindow  mainWindow{appContext};
