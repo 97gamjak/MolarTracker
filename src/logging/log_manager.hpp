6ee562fc4d11e4b8afa79b3d68d7f7b8ce6188d6
Jakob Gamper <97gamjak@gmail.com>
2026-01-12 23:17:31 +0100

    feat: implement logging framework with LogManager and LogStream classes - first approach only backbone implemented

---
+++ src/logging/log_manager.hpp
@@ -0,0 +1,24 @@
+#ifndef __LOGGING__LOG_MANAGER_HPP__
+#define __LOGGING__LOG_MANAGER_HPP__
+
+class LogManager
+{
+   public:
+    static LogManager& getInstance();
+
+    bool isEnabled(const LogCategory& category, LogLevel level) const;
+
+    void registerBuiltInCategories();
+    void registerCategory(LogCategoryId id, std::string_view name);
+
+    void log(
+        LogLevel           level,
+        const LogCategory& category,
+        const char*        file,
+        int                line,
+        const char*        function,
+        const std::string& message
+    );
+};
+
+#endif   // __LOGGING__LOG_MANAGER_HPP__
