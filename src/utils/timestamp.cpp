a4ad18916d690a02a6728fcb000cfb5cb9104fa9
Jakob Gamper <97gamjak@gmail.com>
2026-01-24 17:15:25 +0100

    feat: update changelog structure and enhance timestamp utility with timezone support

--- src/utils/timestamp.cpp
+++ src/utils/timestamp.cpp
@@ -1,16 +1,27 @@
 #include "utils/timestamp.hpp"
 
+using std::chrono::current_zone;
 using std::chrono::duration_cast;
 using std::chrono::floor;
 using std::chrono::milliseconds;
 using std::chrono::seconds;
+using std::chrono::zoned_time;
 
 /**
  * @brief Returns the current time point from the system clock
  *
  * @return Timestamp::TimePoint
  */
-Timestamp::TimePoint Timestamp::now() { return Clock::now(); }
+Timestamp::TimePoint Timestamp::now()
+{
+    const auto now = Clock::now();
+
+    const auto* tz = current_zone();
+
+    zoned_time local_time{tz, now};
+
+    return local_time.get_local_time();
+}
 
 /**
  * @brief Returns the current Unix timestamp in seconds
