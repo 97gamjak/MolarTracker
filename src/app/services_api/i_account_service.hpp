56cf9019c457b9c69ce13b781a03054d411c13ad
Jakob Gamper <97gamjak@gmail.com>
2026-03-10 11:52:29 +0100

    MOLTRACK-188 Implement AccountService and IAccountService for cash account management

---
+++ src/app/services_api/i_account_service.hpp
@@ -0,0 +1,39 @@
+#ifndef __APP__SERVICES_API__I_ACCOUNT_SERVICE_HPP__
+#define __APP__SERVICES_API__I_ACCOUNT_SERVICE_HPP__
+
+#include "config/id_types.hpp"
+
+namespace finance
+{
+    class CashAccount;   // forward declaration
+}   // namespace finance
+
+namespace app
+{
+    /**
+     * @brief Interface for Account service
+     *
+     */
+    class IAccountService
+    {
+       public:
+        virtual ~IAccountService() = default;
+
+        /**
+         * @brief Create a new cash account, this method takes a CashAccount
+         * domain object as input and creates a corresponding entry in the
+         * database, it returns the ID of the newly created cash account.
+         *
+         * @param cashAccount The CashAccount domain object containing the
+         * details of the cash account to be created
+         * @return AccountId The ID of the newly created cash account
+         */
+        [[nodiscard]]
+        virtual AccountId createCashAccount(
+            const finance::CashAccount& cashAccount
+        ) = 0;
+    };
+
+}   // namespace app
+
+#endif   // __APP__SERVICES_API__I_ACCOUNT_SERVICE_HPP__
