7088afbbedc822734dc77b2a004f2bb6f21f0504
Jakob Gamper <97gamjak@gmail.com>
2026-04-05 21:35:28 +0200

    MOLTRACK-221 fix: update include guards in ITransactionService header file

--- src/app/include/app/services_api/i_transaction_service.hpp
+++ src/app/include/app/services_api/i_transaction_service.hpp
@@ -1,5 +1,5 @@
-#ifndef __APP__SERVICES_API__I_TRANSACTION_SERVICE_HPP__
-#define __APP__SERVICES_API__I_TRANSACTION_SERVICE_HPP__
+#ifndef __APP__INCLUDE__APP__SERVICES_API__I_TRANSACTION_SERVICE_HPP__
+#define __APP__INCLUDE__APP__SERVICES_API__I_TRANSACTION_SERVICE_HPP__
 
 #include "finance/transaction.hpp"
 
@@ -27,4 +27,4 @@ namespace app
     };
 }   // namespace app
 
-#endif   // __APP__SERVICES_API__I_TRANSACTION_SERVICE_HPP__
+#endif   // __APP__INCLUDE__APP__SERVICES_API__I_TRANSACTION_SERVICE_HPP__
